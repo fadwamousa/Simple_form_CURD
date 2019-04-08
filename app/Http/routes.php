6a80343cf4a9c395a5817ec6c6aad8614ee17ba1
fadwa <fadwaana99@gmail.com>
2019-04-08 09:54:39 +0200

    Carbon

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,10 +1,28 @@
 <?php
 
-
-
+use Carbon\Carbon;
 Route::get('/', function () {
     return view('welcome');
 });
 
 
 Route::resource('/posts','PostsController');
+
+Route::get('/dates',function()
+{
+	$date = new DateTime();
+	echo $date->format('M-D-Y');
+	echo "<br/>";
+
+	echo Carbon::now();
+
+	echo "<br/>";
+
+	echo Carbon::now()->diffForHumans();
+    
+    echo "<br/>";
+	echo Carbon::now()->addDay(10)->diffForHumans();
+	echo "<br/>";
+
+	echo Carbon::yesterday()->diffForHumans();
+});
